1a7e8fb1c5edefdad6ac32f780f32dd5a5cb47e9
Taylor Becker <taylor.becker@dotrun.io>
2021-07-04 19:08:18 -0400

    Combine Breed and Descriptor classes and simplify fish info factory

--- src/main/kotlin/net/shotbow/interestingfish/FishInfoFactory.kt
+++ src/main/kotlin/net/shotbow/interestingfish/FishInfoFactory.kt
@@ -2,40 +2,25 @@ package net.shotbow.interestingfish
 
 import net.kyori.adventure.text.minimessage.MiniMessage
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer
-import net.shotbow.interestingfish.InterestingFish.Companion.log
 import net.shotbow.interestingfish.config.InterestingConfig
 import net.shotbow.interestingfish.models.Breed
 import net.shotbow.interestingfish.models.Descriptor
 import net.shotbow.interestingfish.models.FishInfo
+import net.shotbow.interestingfish.models.FishModifier
 import org.bukkit.ChatColor
 import kotlin.random.Random
 
 class FishInfoFactory(private val config: InterestingConfig) {
-    private val weightedDescriptors: MutableList<String> = mutableListOf()
-    private val descriptorIndex: MutableMap<String, Descriptor> = mutableMapOf()
-    private val weightedBreeds: MutableList<String> = mutableListOf()
-    private val breedIndex: MutableMap<String, Breed> = mutableMapOf()
-
-    init {
-        for (descriptor in config.descriptors) {
-            descriptorIndex[descriptor.text] = descriptor
-            repeat(descriptor.rollWeight) { weightedDescriptors.add(descriptor.text) }
-        }
-        for (breed in config.breeds) {
-            breedIndex[breed.text] = breed
-            repeat(breed.rollWeight) { weightedDescriptors.add(breed.text) }
-            for (i in 0..breed.rollWeight) weightedBreeds.add(breed.text)
-        }
-        log(descriptorIndex.size.toString() + " Descriptors & " + breedIndex.size + " Breeds loaded")
-    }
+    private val weightedDescriptors: Array<Descriptor> = config.descriptors.toWeightedArray()
+    private val weightedBreeds: Array<Breed> = config.breeds.toWeightedArray()
 
     fun makeNewFishInfo(): FishInfo {
-        val breed = breedIndex[weightedBreeds.random()]!!
+        val breed = weightedBreeds.random()
         var weight = config.baseWeight + breed.weight
         var name = breed.text.withColors()
 
         if (Random.nextInt(100) < config.percentDescriptorChance) {
-            val descriptor = descriptorIndex[weightedDescriptors.random()]!!
+            val descriptor = weightedDescriptors.random()
 
             name = "${descriptor.text.withColors()}${ChatColor.RESET} $name"
             weight += descriptor.weight
@@ -50,5 +35,8 @@ class FishInfoFactory(private val config: InterestingConfig) {
 
         fun String.withColors() =
             legacyHexSerializer.serialize(MiniMessage.get().parse(replace('&', ChatColor.COLOR_CHAR)))
+
+        fun Iterable<FishModifier>.toWeightedArray() =
+            flatMap { modifier -> Array(modifier.rollWeight) { modifier }.toList() }.toTypedArray()
     }
 }
